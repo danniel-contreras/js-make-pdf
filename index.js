4a226060baaf3996faea303a7a1632f22f84e133
Manuelhm09 <Jmanuelhm09@outlook.com>
2023-02-28 14:45:35 -0600

    deploy for second

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require("express")
-const cors = require("cors")
-const {router } = require("./src/routes/router")
+const express = require("express");
+const cors = require("cors");
+const { router } = require("./src/routes/router");
 
-const app = express()
+const app = express();
 
-app.use(express.json({ limit: "50mb" }))
-app.use(express.urlencoded({ limit: "50mb", extended: true }))
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors({ origin: "*" }))
+app.use(
+  cors({
+    origin: [
+      "https://afraco-admin-app-5sl9b.ondigitalocean.app",
+      "http://localhost:5173",
+    ],
+  })
+);
 
-app.use(router)
-
+app.use(router);
 
 app.listen(3004, () => {
-    console.log("app running at port 3004")
-})
+  console.log("app running at port 3004");
+});
